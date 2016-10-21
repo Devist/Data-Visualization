a41194799763f44be4ddce642bfbb0b32c2865fd
pranker <aboutdh89@naver.com>
2016-10-21 15:17:16 +0900

    소스코드정리

--- Samples/simple/mapCtrl.js
+++ Samples/simple/mapCtrl.js
@@ -1,4 +1,5 @@
 var map;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 36.043501, lng: -75.520654},
@@ -16,12 +17,6 @@ function hideFrontLayer() {
   document.getElementById('frontlayer').style.visibility='hidden';
 }
 
-
-
 $(document).ready( function() {
-
 	$("#result").load("dashboard.html");
-
-	//~이렇게 한줄만 해주면 알아서 contents에 testContents.html파일을 넣어 준다.
-
 });
--- Samples/simple2/index.js
+++ Samples/simple2/index.js
@@ -19,9 +19,5 @@ function hideFrontLayer() {
 
 
 $(document).ready( function() {
-
 	$("#result").load("test.html");
-
-	//~이렇게 한줄만 해주면 알아서 contents에 testContents.html파일을 넣어 준다.
-
 });
